4033539881887211ed5c0b939291ff70423cc961
D34Iupick <iupick_d34@gmail.com>
2025-03-21 17:27:08 -0600

    tuneadndolo un poco

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import CardCurso from './components/card-curso';
-import { Button } from '@mui/material';
+
 
 const anteHeroes = [
   { 
     id:1,
-    userName:"Wolverin", 
+    userName:"Wolverin",
+    name: "Logan", 
     UserNameImg:"wolverin", 
-    description:"Wolverin", 
+    description:"Es un mutante con garras retráctiles de adamantium y un poderoso factor de curación que lo hace casi inmortal. Es miembro de los X-Men y tiene un carácter rudo y solitario. Su pasado misterioso y su lucha interna entre su naturaleza animal y su humanidad son aspectos clave de su personaje.", 
     isFollowing: true
   },
   { 
     id:2,
-    userName:"Deadpool", 
+    userName:"Deadpool",
+    name: "Wede Wilson", 
     UserNameImg:"deadpool", 
-    description:"Deadpool", 
+    description:"Es un mercenario con un sentido del humor irreverente y rompe la cuarta pared (sabe que es un personaje de cómic). Tras ser sometido a un experimento que le otorgó un factor curativo similar al de Wolverine, su cuerpo quedó desfigurado. Es conocido por su personalidad caótica, su habilidad con las espadas y su constante charla.", 
     isFollowing: true
   },
   { 
     id:3,
-    userName:"Punisher", 
+    userName:"Punisher",
+    name: "Frank Castle", 
     UserNameImg:"punisher", 
-    description:"Punisher", 
+    description:"Es un vigilante antiheroico que lucha contra el crimen de manera brutal y violenta. Tras la muerte de su familia a manos de la mafia, Frank Castle decide dedicar su vida a exterminar criminales. No tiene superpoderes, pero es un experto en combate, armas y tácticas militares. Su símbolo es una calavera en su pecho.", 
     isFollowing: false
   },
 ]
@@ -28,18 +31,17 @@ const anteHeroes = [
 export default function App() {
  
   return (
-     <>
-        
-          <div className='grid auto-cols-max grid-flow-col'>
+     
+          <div className='w-screen flex flex-wrap justify-center border-amber-600 border-4 bg-cyan-400'>
             {
               anteHeroes.map((anteHeroes) => (
                 <CardCurso key={anteHeroes.id} useNameImg={anteHeroes.UserNameImg} useDescription={anteHeroes.description} isFollowing={anteHeroes.isFollowing}>
                   <strong>{anteHeroes.userName}</strong>
+                  <p>{anteHeroes.name}</p>
                 </CardCurso>  
               ))
             }
           </div>
-          <Button variant="outlined" size="xl">Cambiar descripción</Button>
-        </>
+        
   )
 }
--- src/components/card-curso.jsx
+++ src/components/card-curso.jsx
@@ -9,29 +9,29 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import { Button, CardActions } from '@mui/material';
 
 
-export default function CardCurso({useNameImg, children, useDescription='No hay descrpcion'}) {
+export default function CardCurso({useNameImg, name, children, useDescription='No hay descrpcion'}) {
   
   // console.log(isFollowing);
 
   const [isFollowing, setIsFollowing] = useState(false);
- 
+
   const handleClick = () => {
     setIsFollowing(!isFollowing);
   }
 
-  
   const txtBtn = isFollowing ? 'Siguiendo' : 'Seguir';
   const btnState = isFollowing ? 'outlined' : 'contained';
+  
   const MyImg = `http://localhost:5173/src/assets/imgs/${useNameImg}.jpg`
-  console.log(isFollowing);
+
   return (
 
-    <Card sx={{ maxWidth: 400, alignItems: 'center', margin: 2, borderStyle: 'solid', borderColor: 'blue-500', borderWidth: 2 }}>
+    <Card sx={{ maxWidth: 400, minWidth:250, alignItems: 'center', margin: 2, borderStyle: 'solid', borderColor: 'blue-500', borderWidth: 2 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           image={MyImg}
-          alt="green iguana"
+          alt={useDescription}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
@@ -51,8 +51,7 @@ export default function CardCurso({useNameImg, children, useDescription='No hay 
         <Button>
            Cambiar descrpción
         </Button>
-        </ButtonGroup>
-        
+        </ButtonGroup>     
       </CardActions>
     </Card>
 
